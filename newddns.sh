c5c1442555f5c9792eb82b84b4aa4379d9436be9
garhing <37818717+garhing@users.noreply.github.com>
2019-03-05 22:27:59 +0800

    Update newddns.sh

--- newddns.sh
+++ newddns.sh
@@ -7,19 +7,20 @@ echo "#############################################################"
 echo "# One click Install cfddns                                  #"
 echo "# Author: garhing                                           #"
 echo "#############################################################"
-echo "请输入 CFKEY > " ;read cfkey
-echo "请输入 CFUSER > " ;read cfuser
-echo "请输入 CFZONE_NAME > " ;read cfname
-echo "请输入 CFRECORD_NAME > " ;read record
+echo "请输入您的APIKEY： " ;read cfkey
+echo "请输入您的邮箱： " ;read cfuser
+echo "请输入您的主域名： " ;read cfname
+echo "请输入域名记录：" ;read record
 echo Starting.....
 cd /root
 wget  -N --no-check-certificate https://raw.githubusercontent.com/garhing/cfddns/master/cfddns.sh >/dev/null 2>&1
 echo 正在配置cfddns
-sed 's/CFKEY=.*/& $cfkey/g' cfddns.sh
-sed 's/CFUSER=.*/& $cfuser/g' cfddns.sh
-sed 's/CFZONE_NAME=.*/& $cfname/g' cfddns.sh
-sed 's/CFRECORD_NAME=.*/& $record/g' cfddns.sh
-chmod +x cfddns.sh 
-echo '*/2 * * * * /root/cfddns.sh >> /var/log/cfddns.log 2>&1' >> crontab 
-./cfddns.sh
+sed -i -e "s/CFKEY=/$cfkey/g" cfddns.sh
+sed -i -e "s/CFUSER=/$cfuser/g" cfddns.sh
+sed -i -e "s/CFZONE_NAME=/$cfname/g" cfddns.sh
+sed -i -e "s/CFRECORD_NAME=/$record/g" cfddns.sh
+echo 'ddns配置完成'
+chmod +x cfddns.sh && ./cfddns.sh
+echo '设置定时任务'
+echo "*/2 * * * * /root/cfddns.sh >> /var/log/cfddns.log 2>&1" >> /etc/crontab
 echo 配置完成
