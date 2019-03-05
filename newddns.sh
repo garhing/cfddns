8560a50666fb368bcac220ab65c82c69df64c6c0
garhing <37818717+garhing@users.noreply.github.com>
2019-03-05 22:52:39 +0800

    Update newddns.sh

--- newddns.sh
+++ newddns.sh
@@ -15,10 +15,10 @@ echo Starting.....
 cd /root
 wget  -N --no-check-certificate https://raw.githubusercontent.com/garhing/cfddns/master/cfddns.sh >/dev/null 2>&1
 echo 正在配置cfddns
-sed -i -e "s/CFKEY=/$/&cfkey/g" cfddns.sh
-sed -i -e "s/CFUSER=/$/&cfuser/g" cfddns.sh
-sed -i -e "s/CFZONE_NAME=/$/&cfname/g" cfddns.sh
-sed -i -e "s/CFRECORD_NAME=/$/&record/g" cfddns.sh
+sed -i -e "s/CFKEY=/CFKEY=$cfkey/g" cfddns.sh
+sed -i -e "s/CFUSER=/CFUSER=$cfuser/g" cfddns.sh
+sed -i -e "s/CFZONE_NAME=/CFZONE_NAME=$cfname/g" cfddns.sh
+sed -i -e "s/CFRECORD_NAME=/CFRECORD_NAME=$record/g" cfddns.sh
 echo 'ddns配置完成'
 chmod +x cfddns.sh && ./cfddns.sh
 echo '设置定时任务'
